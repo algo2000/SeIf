aa67e03aed251c624f721aa9399bc8b4b09cf913
leo <algo2000@naver.com>
2021-03-14 23:43:06 +0900

    update styled-components props typescript

--- seif-app/components/button/style.tsx
+++ seif-app/components/button/style.tsx
@@ -1,20 +1,27 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const DefaultButton = styled.button`
+interface CounterButtonType {
+  positive? : boolean;
+  negative? : boolean;
+}
+
+export const DefaultButton = styled.button<CounterButtonType>`
   height: 25px;
   color: white;
   border-radius: 8px;
   border: none;
   cursor: pointer;
-  background: linear-gradient(145deg, 
-                              ${(p: {ver: string}) => p.ver == "negative" ? '#ff7575' : '#5bc6ff'},
-                              ${(p: {ver: string}) => p.ver == "negative" ? '#e66262' : '#4da7db'});
-`
+  ${props => props.positive && 
+    css`
+      background: linear-gradient(145deg, #5bc6ff, #4da7db);
+  `};
 
-export const NegativeButton = styled(DefaultButton)`
-  background: linear-gradient(145deg, #ff7575, #e66262);
-`
+  ${props => props.negative && 
+    css`
+      background: linear-gradient(145deg, #ff7575, #e66262);
+  `};
 
-export const PositiveButton = styled(DefaultButton)`
-  background: linear-gradient(145deg, #5bc6ff, #4da7db);
+  &:hover {
+    opacity: 80%;
+  }
 `
--- seif-app/features/counter/Counter.tsx
+++ seif-app/features/counter/Counter.tsx
@@ -9,11 +9,11 @@ export const Counter = () => {
   
   return (
     <>
-      <DefaultButton ver="positive" onClick={() => dispatch(increment())}>Increment</DefaultButton>
-      <DefaultButton ver="negative" onClick={() => dispatch(decrement())}>Decrement</DefaultButton>
+      <DefaultButton positive onClick={() => dispatch(increment())}>Increment</DefaultButton>
+      <DefaultButton negative onClick={() => dispatch(decrement())}>Decrement</DefaultButton>
       <br/>
-      <DefaultButton ver="positive" onClick={() => dispatch(incrementByAmount({value : 10}))}>IncrementByAmount</DefaultButton>
-      <DefaultButton ver="negative" onClick={() => dispatch(decrementByAmount({value : 10}))}>DecrementByAmount</DefaultButton>
+      <DefaultButton positive onClick={() => dispatch(incrementByAmount({value : 10}))}>IncrementByAmount</DefaultButton>
+      <DefaultButton negative onClick={() => dispatch(decrementByAmount({value : 10}))}>DecrementByAmount</DefaultButton>
       <p>값 : {state.value}</p>
     </>
   );
