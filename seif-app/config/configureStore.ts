f82014bf2d6539eef869038710e1b4d07c17b567
leo <algo2000@naver.com>
2021-03-25 23:49:40 +0900

    update redux wrapper

--- seif-app/config/configureStore.ts
+++ seif-app/config/configureStore.ts
@@ -14,7 +14,6 @@ const preloadedState = () => {
 const store = () => {
   return configureStore({
     reducer: rootReducer,
-    devTools: process.env.NODE_ENV !== 'production',
     preloadedState: preloadedState(),
   });
 };
