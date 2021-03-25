3017896bb9a14a65516ddc260d2a8de43b588647
leo <algo2000@naver.com>
2021-03-25 23:49:27 +0900

    update redux wrapper setting

--- seif-app/pages/_app.tsx
+++ seif-app/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { AppProps } from "next/app";
 import React from "react";
-import { Provider } from "react-redux";
-import store, { wrapper } from "../config/configureStore";
+import { wrapper } from "../config/configureStore";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider store={store()}>
+    <>
       <Component {...pageProps} />
-    </Provider>
+    </>
+    //
+    // <Provider store={store()}>
+    //   <Component {...pageProps} />
+    // </Provider>
   );
 };
 
-export default wrapper.withRedux(MyApp);;
+export default wrapper.withRedux(MyApp);
